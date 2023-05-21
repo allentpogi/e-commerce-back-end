ffebdb377a22753fe946c5172650927bb6aba389
allen <allen.tuazon@gmail.com>
2023-05-21 18:12:19 +1000

    fixed a bug i found during the demo

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -73,9 +73,10 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(categoryData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
+    console.log(err);
   }
 });
 
